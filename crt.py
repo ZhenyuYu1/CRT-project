a6dcfacdb50df746aceaa7651045a7facfe58514
Mission Bit <missionbit@36a-MissionBit.local>
2020-10-07 18:28:23 -0700

    added a way to force the player to choose either 1 or 2 for whether to continue playing or not

--- crt.py
+++ crt.py
@@ -4,7 +4,7 @@ playerWins = 0
 computerWins = 0
 numGames = 0
 while(runProgram):
-    user = input("Compost, Recycling, Trash.")
+    user = input("Please choose one of the following: Compost, Recycling, Trash.")
     choice = ["Compost", "Recycling", "Trash"]
     com = random.choice(choice)
     if (user == "Compost" and com == "Compost") or (user == "Recycling" and com == "Recyling") or (user == "Trash" and com == "Trash"):
@@ -26,9 +26,19 @@ while(runProgram):
     print(f'Computer score: {computerWins}, {computerWins/numGames * 100} win rate.')
     print(f'Number of games played: {numGames}')
 
-    play = input("Play again?(Y/N)") 
-    if play == "N":
-        runProgram = False
-    else:
-        runProgram = True
-print("The end.")
+    play = int(input("Enter 1 to play again. Enter 2 to stop playing."))
+    while play != 1:
+        if play == 1:
+            runProgram = True
+            break
+        elif play == 2:
+            runProgram = False
+            break
+        else:
+            play = int(input("Enter 1 to play again. Enter 2 to stop playing."))
+    
+print("Final scores:")
+print(f'Player score: {playerWins}, {playerWins/numGames * 100} win rate.')
+print(f'Computer score: {computerWins}, {computerWins/numGames * 100} win rate.')
+print(f'Number of games played: {numGames}')
+print("Thanks for playing!")
